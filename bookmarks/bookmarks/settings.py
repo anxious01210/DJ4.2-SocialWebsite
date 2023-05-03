92a8afa88a60a243209efa2eb93db16be684f54a
anxious01210 <pinktheory21@gmail.com>
2023-05-03 17:51:00 +0300

    4th: FB/Google/Twitter login with auto profile creation added successfully.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w4kgk^fyxt7#3ta-^-e-6u1*_lnuqy!jox^1o0)_92h0if_o!a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1', ]
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,35 @@ LOGOUT_URL = 'logout'
 
 # to print the email in the terminal only without connecting a real email address to send the emails.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '198559719676807' # Facebook App ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '55a4f1414d7d6e32cb0a80cba5a5b6fe' # Facebook App Secret
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_TWITTER_KEY = 'V26O8z4zjWguKUeDIoeCCibq9' # Twitter API Key
+SOCIAL_AUTH_TWITTER_SECRET = 'w1zaeK4eANwSnqyaqfLfKFhWVHyhQlyNgZBqIli5x4AbXsjErH' # Twitter API Secret
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '68152974168-16v0kurvg9jkm2en2ogtpbtn4o6m0lrk.apps.googleusercontent.com' # Google Client ID
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Google Client Secret
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile', # to Auto create Profile for social-app user creation
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
